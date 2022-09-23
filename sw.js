b4b322b7121ed73a98801d687433a1ce890de777
AsmaaMK <asmaa.m.khalaf.fci@gmail.com>
2022-09-24 00:46:06 +0200

    Add service worker

---
+++ sw.js
@@ -0,0 +1,32 @@
+const cacheName = 'v1';
+const resourcesToPrecache = [
+  '/',
+  '/index.html',
+  '/nav-toggler.js',
+  '/styles.css',
+  '/imgs/arrow-icon.svg',
+  '/imgs/close-icon.svg',
+  '/imgs/hamburger-icon.svg',
+  '/imgs/Logo.webp',
+  '/imgs/footer-logo.webp',
+  '/imgs/promotion-background.webp',
+  '/imgs/recipe-1.webp',
+  '/imgs/recipe-2.webp',
+  '/imgs/recipe-3.webp',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(resourcesToPrecache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
